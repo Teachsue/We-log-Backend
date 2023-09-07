1c4f07c7a8c2a31eeecd0012ea07b359213a7fc8
김상원 <sangwonkim@gimsang-wons-MacBook-Pro.local>
2023-09-07 20:34:41 +0900

    ADD : SignUp

---
+++ services/userService.js
@@ -0,0 +1,29 @@
+const userDao = require("../models/userDao");
+const bcrypt = require("bcrypt");
+
+const signUp = async (name, email, passWord, phoneNumber) => {
+  const pwValidation = new RegExp(
+    "^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[a-z\\d@$!%*?&]{8,20}"
+  );
+  const saltRounds = 12;
+
+  if (!pwValidation.test(passWord)) {
+    const err = new Error("CHECK_PASSWORD");
+    err.statusCode = 400;
+    throw err;
+  }
+
+  const hashedPassword = await bcrypt.hash(passWord, saltRounds);
+  const createUser = await userDao.createUser(
+    name,
+    email,
+    hashedPassword,
+    phoneNumber
+  );
+
+  return createUser;
+};
+
+module.exports = {
+  signUp,
+};
