458feb5fd4479e901028e0da56f0a0d027b6b0a7
김상원 <sangwonkim@gimsang-wons-MacBook-Pro.local>
2023-09-09 20:40:56 +0900

    ADD : getLikePostByMe

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -8,4 +8,6 @@ router.get("/getAllPosts", postController.getAllPosts);
 router.get("/getMyPosts", postController.getMyPosts);
 router.get("/getUserPosts", postController.getUserPosts);
 router.post("/like/:postId", postController.likePostById);
+router.get("/getLikePostByMe", postController.getLikePostByMe);
+
 module.exports = router;
